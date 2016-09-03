72c58280496c25d67def008e29c76a6d655e7b03
AndreyYeryomin <andrey.yeryomin11@gmail.com>
2016-09-03 19:57:59 +0300

    create dir template

--- js/controler.js
+++ js/controler.js
@@ -4,8 +4,21 @@
 'use strict';
  var productlistApp = angular.module('productlistApp', []);
 
-productlistApp.controller('ListCtrl', function ($scope,$http) {
+// productlistApp.config(['routeProvider'], function ($routeProvider) {
+//    $routeProvider
+//        .when('/product1',{
+//         templateUrl: 'temp/product1',
+//            controller: 'product1Ctrl'
+//     })
+//        .when('/product2',{
+//            templateUrl: 'temp/product2',
+//            controller: 'product2Ctrl'
+//        })
+// });
+
+productlistApp.controller('ListCtrl', function ($scope, $http, $location) {
   $http.get('http://smktesting.herokuapp.com/api/products/').success(function (data, status, headers, config) {
-     console.log(data, status,headers,config); 
+     console.log(data, status,headers,config);
+      $scope.product = data;
   })
 });
