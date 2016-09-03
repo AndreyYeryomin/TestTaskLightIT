cacfe2474e93b9f22b791156a6a02e6e2fd40f63
AndreyYeryomin <andrey.yeryomin11@gmail.com>
2016-09-03 23:17:55 +0300

    add routing

--- js/controler.js
+++ js/controler.js
@@ -2,23 +2,33 @@
  * Created by Andrey on 03.09.2016.
  */
 'use strict';
- var productlistApp = angular.module('productlistApp', []);
+ var productlistApp = angular.module('productlistApp', ['ngRoute']);
 
-// productlistApp.config(['routeProvider'], function ($routeProvider) {
-//    $routeProvider
-//        .when('/product1',{
-//         templateUrl: 'temp/product1',
-//            controller: 'product1Ctrl'
-//     })
-//        .when('/product2',{
-//            templateUrl: 'temp/product2',
-//            controller: 'product2Ctrl'
-//        })
-// });
+ productlistApp.config(['$routeProvider', function ($routeProvide) {
+     $routeProvide
+         .when('/product1', {
+             templateUrl: 'temp/product1.html',
+             controller: 'product1Ctrl'
+         })
+         .when('/product2', {
+             templateUrl: 'temp/product2.html',
+             controller: 'product2Ctrl'
+         })
+         .otherwise({
+         redirectTo: '/'
+     });
+ }
+ ]);
 
-productlistApp.controller('ListCtrl', function ($scope, $http, $location) {
+productlistApp.controller('ListCtrl', function ($scope, $http) {
   $http.get('http://smktesting.herokuapp.com/api/products/').success(function (data, status, headers, config) {
      console.log(data, status,headers,config);
       $scope.product = data;
   })
+});
+
+productlistApp.controller('product1Ctrl', function ($scope, $http, $location) {
+
+});
+productlistApp.controller('product2Ctrl', function ($scope, $http, $location) {
 });
