b55f8dae157be8b2db6f2d9dc5cafa82483f1a4b
AndreyYeryomin <andrey.yeryomin11@gmail.com>
2016-09-04 20:35:56 +0300

    implemented authorization and added reviews

--- js/controler.js
+++ js/controler.js
@@ -7,6 +7,7 @@
 productlistApp.run(['$http', '$cookies', function ($http, $cookies){
     $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
     $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
 }]);
 
  productlistApp.config(['$routeProvider', function ($routeProvide) {
@@ -40,23 +41,54 @@ productlistApp.controller('product1Ctrl', function ($scope, $http, $location) {
         $scope.reviews1 = data;
     })
 });
+
 productlistApp.controller('product2Ctrl', function ($scope, $http, $location) {
     $http.get('http://smktesting.herokuapp.com/api/reviews/2').success(function (data, status, headers, config) {
         $scope.reviews2 = data;
     })
 });
+
 productlistApp.controller('registrationCtrl', function ($scope, $http, $location) {
     $scope.newName = "";
     $scope.newPass = "";
-    $scope.sendPost = function() {
+    $scope.regUser = function() {
         var data = $.param({
                 username: $scope.newName,
                 password: $scope.newPass
         });
         console.log(data);
         $http.post("http://smktesting.herokuapp.com/api/register/", data).success(function(data, status) {
-            console.log(status);
-            console.log(data);
+
+        })
+
+    }
+});
+productlistApp.controller('signInCtrl', function ($scope, $http, $location, $cookies) {
+    $scope.userName = "";
+    $scope.userPass = "";
+    $scope.signIn = function() {
+        var data = $.param({
+            username: $scope.userName,
+            password: $scope.userPass
+        });
+        $http.post("http://smktesting.herokuapp.com/api/login/", data).success(function (data, status) {
+            $http.defaults.headers.post['Authorization']="Token " + data.token;
+        });
+
+    }
+
+});
+productlistApp.controller('addReviews', function ($scope, $http, $location) {
+    $scope.rate = "";
+    $scope.text = "";
+    $scope.addComments = function() {
+        var data = $.param({
+            rate: $scope.rate,
+            text: $scope.text
+        });
+        console.log(data);
+        $http.post("http://smktesting.herokuapp.com/api/reviews/1", data).success(function(data, status) {
+
         })
 
     }
